66f5b75d2ac9f4a8c90161548ff61f5c29887472
Samuel <s.olubode@alustudent.com>
2023-06-30 14:20:24 +0100

    Removes some folders

--- dist/utils/interfaces.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// export interface CustomError extends {
-//     statusCode: Number;
-// }
--- src/utils/interfaces.ts
+++
@@ -1,4 +0,0 @@
-
-// export interface CustomError extends {
-//     statusCode: Number;
-// }
